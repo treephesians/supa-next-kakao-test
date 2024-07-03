06c3442f490833ef9132a08a991a84176646a8de
Junbeom Park <ephesi4ns623@gmail.com>
2024-07-04 01:25:23 +0900

    add Logout

--- app/components/KakaoLoginButton.tsx
+++ app/components/KakaoLoginButton.tsx
@@ -7,28 +7,48 @@ import { ThemeSupa } from "@supabase/auth-ui-shared";
 import { createClient } from "@supabase/supabase-js";
 import { supabase } from "@/lib/client/supabaseClient";
 import useHydrate from "@/hooks/useHydrate";
+import { User } from "@supabase/supabase-js";
 
 const KakaoLoginButton = () => {
   // const handleLogin = async () => {
   //   const { data } = await signInKakao();
   // };
-  const [name, setName] = useState(false);
+  // const [user, setUser] = useState<User | null>(null);
+  const [name, setName] = useState<string | null>(null);
   const isMount = useHydrate();
+
   const getUserInfo = async () => {
     const result = await supabase.auth.getUser();
     const name = result.data.user?.user_metadata.name;
-    setName(name);
+    // name 이 아니라 user을 받아야 하는데 DataType을 추가해줘야 하는듯
+    //if (result?.data?.user) setUser(result?.data?.user);
+    if (name) setName(name);
+  };
+
+  const handleLogout = async () => {
+    supabase.auth.signOut();
+    setName(null);
+    window.location.reload();
   };
 
   useEffect(() => {
     getUserInfo();
   }, []);
+
   if (!isMount) return null;
+
   return (
     //<button onClick={handleLogin}>카카오톡으로 로그인</button>;
     <div className="mx-auto max-w-[500px]">
       <div className="text-center text-2xl font-bold">PrayU</div>
-      {name && <div className="text-center text-xl mt-4">Hello {name}</div>}
+      {name && (
+        <div className="text-center text-xl mt-4">
+          Hello {name}
+          <button onClick={handleLogout} className="border-2 border-black ml-4">
+            Logout
+          </button>
+        </div>
+      )}
       <Auth
         redirectTo="process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO"
         supabaseClient={supabase}
